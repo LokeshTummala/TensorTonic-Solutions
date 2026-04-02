47c7df40b25c25c4cf97c184704435de166c55e6
LokeshTummala <129769362+LokeshTummala@users.noreply.github.com>
2026-04-02 21:41:59 +0530

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -6,4 +6,11 @@ def sigmoid(x):
     """
     # Write code here
     x = np.array(x)
-    return 1/(1+np.exp(-x))
+    return np.where(x>0, 1/(1+np.exp(-x)),np.exp(x)/(1+np.exp(x)))
+    
+    """
+    If x is very large negative then use e^x/(1+e^x) as the function and if x is a very large positive then it works fine so use the normal one.
+    
+    Alternative : return np.where(x>0,1/(1+np.exp(-x)),np.exp(x)/(1+np.exp(x)))
+    
+    """
