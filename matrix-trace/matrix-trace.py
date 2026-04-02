abd184492202570aaa7a3043f61fd99ab0451ab1
LokeshTummala <129769362+LokeshTummala@users.noreply.github.com>
2026-04-02 22:23:34 +0530

    Update solution for matrix-trace

--- matrix-trace/matrix-trace.py
+++ matrix-trace/matrix-trace.py
@@ -5,6 +5,7 @@ def matrix_trace(A):
     Compute the trace of a square matrix (sum of diagonal elements).
     """
     # Write code here
-    n = len(A)
-    trace = sum(A[i][i] for i in range(len(A)))
-    return trace
+    #n = len(A)
+    #trace = sum(A[i][i] for i in range(len(A)))
+    #return trace
+    return np.trace(A)
