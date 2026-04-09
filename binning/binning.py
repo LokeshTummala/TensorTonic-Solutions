f4f422c02deb9d1069959e5ccc945e0f656fd129
LokeshTummala <129769362+LokeshTummala@users.noreply.github.com>
2026-04-09 11:40:10 +0530

    Update solution for binning

--- binning/binning.py
+++ binning/binning.py
@@ -6,11 +6,11 @@ def binning(values, num_bins):
     min_val = np.min(values)
     max_val = np.max(values)
     
-    # 🚨 Edge case: all values same
+    #  Edge case: all values same
     if min_val == max_val:
         return [0] * len(values)
-    
-    w = (max_val - min_val) / num_bins
+    range_of_x = max_val - min_val
+    w = (range_of_x) / num_bins
     
     bins = np.floor((values - min_val) / w)
     bins = np.clip(bins, 0, num_bins - 1)
